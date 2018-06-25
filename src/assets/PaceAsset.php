57b0caaccc3887f374a9396cb1a8bb947f6f253e
Nick Panteleeff <drnix@chess-online.com>
2018-06-25 18:03:12 +0300

    Some fixes

--- src/assets/PaceAsset.php
+++ src/assets/PaceAsset.php
@@ -5,23 +5,6 @@ use yii\web\AssetBundle;
 
 class PaceAsset extends AssetBundle
 {
-    /**
-     * Color theme
-     * Available: black, blue, green, orange, pink, purple, red, silver, white, yellow
-     *
-     * @var string
-     */
-    public $color = "orange";
-
-    /**
-     * Animation
-     * Available: barber-shop, big-counter, bounce, center-atom, center-circle, center-radar, center-simple,
-     * corner-indicator, fill-left, flash, flat-top, loading-bar, mac-osx, minimal
-     *
-     * @var string
-     */
-    public $animation = "flash";
-
     public $sourcePath = '@bower/pace';
 
     public $css = [
@@ -34,22 +17,4 @@ class PaceAsset extends AssetBundle
 
     public $depends = [
     ];
-
-    public function init()
-    {
-        parent::init();
-
-        $this->css = [
-            "themes/{$this->color}/pace-theme-{$this->animation}.css"
-        ];
-
-        $this->publishOptions['beforeCopy'] = function ($from, $to) {
-            if (is_dir($from)) {
-                return true;
-            } else {
-                $ext = pathinfo($from, PATHINFO_EXTENSION);
-                return $ext == "js" || $ext == "css";
-            }
-        };
-    }
 }
